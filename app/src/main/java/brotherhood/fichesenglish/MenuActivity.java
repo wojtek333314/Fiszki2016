6f0e5fb03aa4c03b785d1cd7a591eaf811b08890
Przemysław Kudła <przemyslawkudla94@gmail.com>
2016-02-08 20:26:02 +0100

    init

---
+++ app/src/main/java/brotherhood/fichesenglish/MenuActivity.java
@@ -0,0 +1,13 @@
+package brotherhood.fichesenglish;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
