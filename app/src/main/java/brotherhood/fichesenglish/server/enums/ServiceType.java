55b9e9c5398ea09b8fc0d8cd7e37f4ea8aba8c2f
Przemysław Kudła <przemyslawkudla94@gmail.com>
2016-02-16 20:50:12 +0100

    Splash screen

--- app/src/main/java/brotherhood/fichesenglish/server/enums/ServiceType.java
+++ app/src/main/java/brotherhood/fichesenglish/server/enums/ServiceType.java
@@ -2,16 +2,16 @@ package brotherhood.fichesenglish.server.enums;
 
 
 public enum ServiceType {
-    GET_LIST,
+    GET_DATABASE,
     REGISTER_NICK,
     REGISTER_POINTS;
 
-    private static final String SERVER_PATH = "http://www.datastore.waw.pl/MolesWar/queueSystem/";
+    private static final String SERVER_PATH = "http://www.datastore.waw.pl/Fiche2016/getDatabase.php/";
 
     public static String getURL(ServiceType serviceType){
         switch(serviceType){
 
-            case GET_LIST:
+            case GET_DATABASE:
                 return SERVER_PATH + "getList.php";
             case REGISTER_NICK:
                 return SERVER_PATH + "registerNick.php";
