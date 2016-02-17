4647c6814605d691df9829968a2bb636c29da548
Przemysław Kudła <przemyslawkudla94@gmail.com>
2016-02-17 22:10:51 +0100

    Checking database version

--- app/src/main/java/brotherhood/fichesenglish/activities/SplashActivity.java
+++ app/src/main/java/brotherhood/fichesenglish/activities/SplashActivity.java
@@ -18,49 +18,62 @@ public class SplashActivity extends BaseActivity {
     protected void customOnCreate() {
         setContentView(R.layout.activity_splash);
         System.out.println("ON CREATE");
-        downloadDatabase();
+        checkDatabaseVersion();
     }
 
     public void downloadDatabase() {
-        new ServerRequest(ServiceType.GET_DATABASE, new Parameters().addParam("", ""))
+        new ServerRequest(ServiceType.GET_DATABASE, new Parameters())
                 .setServerRequestListener(new ServerRequest.ServerRequestListener() {
-            @Override
-            public void onSuccess(final String json) {
-                new Thread(new Runnable() {
                     @Override
-                    public void run() {
-                        try {
-                            JSONArray jsonArray = new JSONObject(json).getJSONArray("data");
-                            for(int i = 0; i < jsonArray.length(); i ++) {
-                                JSONObject singleObject = jsonArray.getJSONObject(i);
-                                getDatabaseHelper().saveSingleFiche(singleObject);
+                    public void onSuccess(final String json) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {System.out.println(getSharedPrefsHelper().getString("version"));
+                                    JSONArray jsonArray = new JSONObject(json).getJSONObject("data").getJSONArray("json");
+                                    JSONObject jsonObject = new JSONObject(json).getJSONObject("data");
+                                    String version = jsonObject.getString("version");
+                                    System.out.println("MOJA POBRANA WERSJA" + version);
+                                    getSharedPrefsHelper().putString("version",version);
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject singleObject = jsonArray.getJSONObject(i);
+                                        getDatabaseHelper().saveSingleFiche(singleObject);
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                Intent toMenuActivity = new Intent(SplashActivity.this, MenuActivity.class);
+                                startActivity(toMenuActivity);
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        }).start();
 
-                        Intent toMenuActivity = new Intent(SplashActivity.this, MenuActivity.class);
-                        startActivity(toMenuActivity);
+
                     }
-                }).start();
 
+                    @Override
+                    public void onError(int code, String description) {
 
-            }
-
-            @Override
-            public void onError(int code, String description) {
-
-            }
-        }).execute();
+                    }
+                }).execute();
     }
 
     public void checkDatabaseVersion() {
-        new ServerRequest(ServiceType.CHECK_VERSION, new Parameters().addParam("","")).
-                setServerRequestListener(new ServerRequest.ServerRequestListener() {
+        new ServerRequest(ServiceType.CHECK_VERSION, new Parameters())
+                .setServerRequestListener(new ServerRequest.ServerRequestListener() {
                     @Override
                     public void onSuccess(String json) {
                         try {
                             JSONObject jsonObject = new JSONObject(json);
+                            String version = jsonObject.getJSONObject("data").getString("version");
+                            String localVersion = getSharedPrefsHelper().getString("version");
+
+                          //  System.out.println(localVersion+":"+version);
+                            if (localVersion != null && localVersion.equals(version)) {
+                                Intent toMenuActivity = new Intent(SplashActivity.this, MenuActivity.class);
+                                startActivity(toMenuActivity);
+                            } else
+                                downloadDatabase();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
